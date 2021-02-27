139e3e9623abe5df2c6bfd0326c3cbf0b6ac0e1a
gerlinp <gerlin.pierrelouis@gmail.com>
2021-02-27 17:47:08 -0500

    Loops

--- javascript-loops/1 - Simplify Repetitive Tasks with Loops/2 - The while Loop/start/js/do-while.js
+++ javascript-loops/1 - Simplify Repetitive Tasks with Loops/2 - The while Loop/start/js/do-while.js
@@ -5,5 +5,5 @@ function getRandomNumber(upper) {
 let counter = 0; 
 do {
     console.log(`The random number is ${getRandomNumber(10)}`);
-    counter += 1;
+    counter ++;
 } while ( counter < 10);
